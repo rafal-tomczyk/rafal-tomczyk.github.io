461e5cb635007ee26bacd9fb7c7586e034c2e9a5
eric <rafal.tomczyk2000@gmail.com>
2025-02-18 11:51:17 +0100

    Updates

--- assets/Footer-CfiwdYLf.js
+++ assets/Footer-CfiwdYLf.js
@@ -1,4 +1,4 @@
-import{c as t,j as e}from"./index-CtwBjhVK.js";/**
+import{c as t,j as e}from"./index-adCrCBLF.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
