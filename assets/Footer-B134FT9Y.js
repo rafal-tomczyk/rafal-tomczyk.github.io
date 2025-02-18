3a819531900e311bb3ff2a574f0ae00d9776f335
eric <rafal.tomczyk2000@gmail.com>
2025-02-18 12:03:27 +0100

    Updates

--- assets/Footer-B134FT9Y.js
+++ assets/Footer-B134FT9Y.js
@@ -1,4 +1,4 @@
-import{c as t,j as e}from"./index-adCrCBLF.js";/**
+import{c as t,j as e}from"./index-mPt1z1OG.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
