bc2955faec0a139c5202ab8a42c30d97ae11d54d
eric <rafal.tomczyk2000@gmail.com>
2025-02-18 10:26:56 +0100

    Updates

---
+++ assets/Footer-C4f_PJl8.js
@@ -0,0 +1,16 @@
+import{c as t,j as e}from"./index-CtwBjhVK.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4",key:"tonef"}],["path",{d:"M9 18c-4.51 2-5-2-7-2",key:"9comsn"}]],c=t("Github",a);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z",key:"c2jq9f"}],["rect",{width:"4",height:"12",x:"2",y:"9",key:"mk3on5"}],["circle",{cx:"4",cy:"4",r:"2",key:"bt5ra8"}]],o=t("Linkedin",n);/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M22 13V6a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h8",key:"12jkf8"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}],["path",{d:"M19 16v6",key:"tddt3s"}],["path",{d:"M16 19h6",key:"xwg31i"}]],s=t("MailPlus",r),h=()=>e.jsxs("footer",{children:[e.jsx("h1",{children:"Kontakt"}),e.jsxs("a",{className:"ft-a",href:"https://github.com/rafal-tomczyk",target:"_blank",rel:"noopener noreferrer",children:[e.jsx(c,{})," Github"]}),e.jsxs("a",{className:"ft-a",href:"https://www.linkedin.com/in/rafał-tomczyk-b79a282a5/",target:"_blank",rel:"noopener noreferrer",children:[e.jsx(o,{})," LinkedIn"]}),e.jsxs("p",{className:"ft-a",children:[e.jsx(s,{})," rafal.tomczyk2000@gmail.com"]})]});export{h as default};
